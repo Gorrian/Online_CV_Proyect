7a91a00a2fc1bf04c40a39bc5945f645aa21c021
Gorrian <gorriansan@hotmail.com>
2023-05-08 18:32:49 +0200

    Acabado Experiencias laborales
    He completado el script para que en responsabilidades coja las experiencias laborales del JSON con informacion de mi curriculum.

--- Script/BuildExpLab.js
+++ Script/BuildExpLab.js
@@ -1,9 +1,37 @@
-function BuildExperiencia(Trabajo){
-    readTextFile("/CV_Information.json", function(text){
+function BuildExperiencia(Trabajo, ID) {
+    readTextFile("/CV_Information.json", function (text) {
         let data = JSON.parse(text)[0]["Experiencia laboral"][Trabajo];
-        for(const key in data){
-            console.log(`${key}: ${data[key]}`);
+        let Listado=document.getElementById(ID);
+        let Result="";
+        for (const key in data) {
+            let Element=data[key];
+            if (Array.isArray(Element)) {
+                let TempElement="";
+                Element.forEach(element => {
+                    TempElement+=element;
+                });
+                Element=TempElement;
+            }
+
+            let AttrID=key.replace(/ /g,"_");
+
+            Result+=("<a data-bs-toggle=\"collapse\" href=\"#"+ID+AttrID+"\" role=\"button\" aria-expanded=\"false\" aria-controls=\"collapseExample\">");
+            Result+=("<li class=\"list-group-item list-group-item-action\">"+key);
+            Result+=("</li>");
+            Result+=("</a>");
+            Result+=("<div class=\"collapse\" id=\""+ID+AttrID+"\">");
+            Result+=("<div class=\"card card-body\">");
+            Result+=(Element);
+            Result+=("</div>");
+            Result+=("</div>");
+            
+           
+
+
+            
         }
+        
+        Listado.innerHTML=Result;
     });
 }
-BuildExperiencia("Practicas de SMX")
+//BuildExperiencia("Practicas de SMX", "Something");
