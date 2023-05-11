e3311bbcf338dac020be55217dabf9696537ba70
Gorrian <gorriansan@hotmail.com>
2023-05-11 17:31:57 +0200

    Acabado footer
    Ya he hecho el footer conteniendo informacion de contacto y links a mi linkedin y github

---
+++ Script/BuildContactInfo.js
@@ -0,0 +1,20 @@
+function BuildContactInfo (ContactInfo, ID){
+    readTextFile("/CV_Information.json", function (text) {
+        let data = JSON.parse(text)[0][ContactInfo];
+        let Listado=document.getElementById(ID);
+        let Result="<h3>Informacion de contacto</h3>";
+        Result+="<ul>";
+        for (const key in data) {
+            let Element=data[key];
+            let AttrID=key.replace(/ /g,"_");
+
+            Result+='<li>'+key+': '+Element+'</li>';
+           
+
+
+            
+        }
+        Result+="</ul>"
+        Listado.innerHTML=Result;
+    });
+}
---
+++ Script/BuildReferencias.js
@@ -0,0 +1,18 @@
+function BuildReferencias (Referencias, ID){
+    readTextFile("/CV_Information.json", function (text) {
+        let data = JSON.parse(text)[0][Referencias];
+        let Listado=document.getElementById(ID);
+        let Result="<h3>Paginas referentes</h3>";
+        for (const key in data) {
+            let Element=data[key];
+            let AttrID=key.replace(/ /g,"_");
+            Result+='<a href="'+data[key]["URL"]+'" target="_blank">';
+            Result+='<img src="'+data[key]["Image"]+'" alt="Imagen no cargada o encontrada" class="me-4">';
+            Result+='</a>';
+
+
+            
+        }
+        Listado.innerHTML=Result;
+    });
+}
