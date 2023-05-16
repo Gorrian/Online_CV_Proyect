050d7cf944f2dfbc550cf695975b8ba1020a9f04
Gorrian <gorriansan@hotmail.com>
2023-05-16 18:10:34 +0200

    Easter egg
    Ya he hecho un easter egg del bayblade, no voy a documentarlo, encuentralo tu mismo incluso tu gorrian de hace varios años probablemente.

--- Script/Bayblade.js
+++ Script/Bayblade.js
@@ -1,12 +1,19 @@
 
 $(document).ready(function () {
+    let audio = new Audio('Bayblade.mp3');
     $("#MyPhoto").hover(function () {
         let el = $(this);
         let timeout = setTimeout(function () {
-            console.log("Hover Element");
+            $("#MyPhoto").attr("src","Images/Bayblade.jpg");
+            
+            $('#MyPhoto').click(function () { 
+                audio.play();
+            });
         }, 10000);
         el.data("timeout", timeout);
     }, function () {
         clearTimeout($(this).removeClass("hover").data("timeout"));
+        $(this).attr("src","Images/PhotoOfMe.jpg");
+        $("#MyPhoto").unbind('click');
     });
 });
